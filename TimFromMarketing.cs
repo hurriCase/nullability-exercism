7877c5f2bf9af624fcf3d49530035c170e760761
Dmitriy Sukharev <dmitriy.sukharev@fullhpltd.com>
2024-05-31 16:20:31 +0300

    Initial commit

---
+++ TimFromMarketing.cs
@@ -0,0 +1,7 @@
+using System;
+
+static class Badge
+{
+    public static string Print(int? id, string name, string department) =>
+        $"{(id != null ? $"[{id}] - " : "")}{name} - {department?.ToUpper() ?? "OWNER"}";
+}
